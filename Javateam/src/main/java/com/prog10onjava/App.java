1ab0b48562fc8b6b5a2eeed783e71c6b1ac8a9b5
abhiram177 <rabhiram.22me@cambridge.edu.in>
2025-05-19 11:40:35 +0530

    my first commit

---
+++ Javateam/src/main/java/com/prog10onjava/App.java
@@ -0,0 +1,13 @@
+package com.prog10onjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
